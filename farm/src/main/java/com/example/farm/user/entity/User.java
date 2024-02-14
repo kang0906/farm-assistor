4d831b828be090a7edc24ba1523bccd68ee504eb
jinkoo kang <jinkoo906@gmail.com>
2024-02-14 21:35:41 +0900

    #1 BaseEntity 추가 및 User entity가 BaseEntity를 상속하도록 수정

--- farm/src/main/java/com/example/farm/user/entity/User.java
+++ farm/src/main/java/com/example/farm/user/entity/User.java
@@ -1,5 +1,6 @@
 package com.example.farm.user.entity;
 
+import com.example.farm.common.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -11,7 +12,7 @@ import java.time.LocalDateTime;
 @Getter
 @Table(name = "MEMBER")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class User {
+public class User extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,7 +25,6 @@ public class User {
     private Long kakaoId;
     private String username;
     private String password;
-    private LocalDateTime joinDate;
     private LocalDateTime lastAccessDate;
 
 
@@ -34,7 +34,6 @@ public class User {
         this.kakaoId = kakaoId;
         this.username = username;
         this.password = password;
-        this.joinDate = LocalDateTime.now();
         this.lastAccessDate = LocalDateTime.now();
     }
 }
